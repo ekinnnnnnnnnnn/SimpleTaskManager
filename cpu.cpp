cc7adcd60a68d589c6f3bb06eaf48a3b1c386f08
ekinnnnnnnnnnn <ekincaglar2004@gmail.com>
2026-06-01 21:18:35 +0300

    smth that not important aq

--- cpu.cpp
+++ cpu.cpp
@@ -38,8 +38,6 @@ struct cpuData {
     long uptime;
     long processes;
     string l3Cache;
-    string microcode;
-    string family;
 };
 
 class cpuMonitor {
@@ -63,7 +61,49 @@ class cpuMonitor {
             }
             return stats;
         }
-};
+        void getFeatures(cpuData &data) {
+            ifstream cpuinfo("/proc/cpuinfo");
+            string line;
+            vector<int> physicalIds;  
+
+            while (getline(cpuinfo, line)) {
+                if (line.find("model name") != string::npos)
+                    data.model = line.substr(line.find(":") + 2);
+
+                else if (line.find("cpu MHz") != string::npos)
+                    data.currentSpeed = stod(line.substr(line.find(":") + 2));
+
+                else if (line.find("cpu cores") != string::npos)
+                    data.cores = stoi(line.substr(line.find(":") + 2));
+
+                else if (line.find("processor") != string::npos)
+                    data.logical = stoi(line.substr(line.find(":") + 2)) + 1;
+
+                else if (line.find("physical id") != string::npos) {
+                    int id = stoi(line.substr(line.find(":") + 2));
+                    if (find(physicalIds.begin(), physicalIds.end(), id) == physicalIds.end())
+                    physicalIds.push_back(id);
+                }
+
+            }
+
+            data.sockets = physicalIds.empty() ? 1 : (int)physicalIds.size();
+
+            ifstream cacheFile("/sys/devices/system/cpu/cpu0/cache/index3/size");
+            cacheFile >> data.l3Cache;
+
+            ifstream uptimeFile("/proc/uptime");
+            uptimeFile >> data.uptime;
+
+            ifstream statFile("/proc/stat");
+            string label;
+            while (statFile >> label) {
+                if (label == "processes")
+                    statFile >> data.processes;
+            }
+        }
+    };
+
 
 
 int main() {
@@ -72,17 +112,25 @@ int main() {
     cpuData data;
 
     stats=monitor.getRawStats();
+    monitor.getFeatures(data);
 
     while (true) {
         this_thread::sleep_for(chrono::seconds(5));
-        cout<<"User: "<<stats.user<<endl
+        /*cout<<"User: "<<stats.user<<endl
             <<"Nice: "<<stats.nice<<endl
             <<"System: "<<stats.system<<endl
             <<"Idle: "<<stats.idle<<endl
             <<"IOWait: "<<stats.iowait<<endl
             <<"IRQ: "<<stats.irq<<endl
             <<"SoftIRQ: "<<stats.softirq<<endl
-            <<"Steal: "<<stats.steal<<endl;
+            <<"Steal: "<<stats.steal<<endl;*/
+        cout<<"Model: "<<data.model<<endl
+            <<"Current Speed: "<<data.currentSpeed<<" MHz"<<endl
+            <<"L3 Cache: "<<data.l3Cache<<endl
+            <<"Sockets: "<<data.sockets<<endl
+            <<"Logical Cores: "<<data.logical<<endl
+            <<"Uptime: "<<data.uptime<<" seconds"<<endl
+            <<"Processes: "<<data.processes<<endl;
     }
 
     return 0;
